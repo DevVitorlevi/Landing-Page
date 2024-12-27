e7f70806882a65d6bf112a0b9db02be24384ba7a
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-27 20:47:37 -0300

    att

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,20 @@
-const Elementos = document.querySelectorAll('.elementos')
+// Seleciona todos os elementos com a classe 'elementos' na página.
+const Elementos = document.querySelectorAll('.elementos');
 
-const Observador = new IntersectionObserver((entrada) => {
-    entrada.forEach(info => {
+// Cria uma nova instância do IntersectionObserver para observar quando os elementos entram na viewport.
+const Observador = new IntersectionObserver((entradas) => {
+    // 'entradas' é um array com informações sobre os elementos observados.
+    entradas.forEach((info) => {
+        // Verifica se o elemento está visível na viewport.
         if (info.isIntersecting) {
-            info.target.classList.add("mostrar")
-        } else {
-            info.target.classList.remove("mostrar")
+            // Se o elemento estiver visível, adiciona a classe 'mostrar'.
+            info.target.classList.add('mostrar');
+            // Para de observar o elemento atual, garantindo que a animação ocorra apenas uma vez.
+            Observador.unobserve(info.target);
         }
-    })
-})
+    });
+});
 
-Elementos.forEach((todos) => Observador.observe(todos))
+// Observa cada elemento selecionado para detectar quando ele entra na viewport.
+Elementos.forEach((todos) => Observador.observe(todos));
+    
