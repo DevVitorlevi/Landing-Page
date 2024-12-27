88188c0eb4aaaebb938d3c91d958a1a3b949c94d
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-27 15:55:51 -0300

    transição do menu

--- js/menumobile.js
+++ js/menumobile.js
@@ -1,24 +1,17 @@
-const MobileBtn = document.getElementById('mobile-btn') 
-const MobileMenu = document.getElementById('mobile-menu') 
-const Burguer = document.querySelector('.fa-bars') 
-const Overlay = document.querySelector('.overlay')
+const MobileBtn = document.getElementById('mobile-btn');
+const MobileMenu = document.getElementById('mobile-menu');
+const Burguer = document.querySelector('.fa-bars');
+const Overlay = document.querySelector('.overlay');
 
+MobileBtn.addEventListener('click', () => {
+    // Alterna a classe 'active' no menu e no overlay
+    MobileMenu.classList.toggle('active');
+    Burguer.classList.toggle('fa-x');
 
-MobileBtn.addEventListener('click', () => { 
-    // Adiciona um evento de clique ao botão `MobileBtn`.
-
-    MobileMenu.classList.toggle('active') 
-    // Alterna a classe 'active' no menu. 
-    // Isso é usado para abrir ou fechar o menu (normalmente associado a um estilo CSS).
-
-    Burguer.classList.toggle('fa-x') 
-    // Alterna entre o ícone de "barras" (`fa-bars`) e o ícone de "X" (`fa-x`).
-    // Isso muda a aparência do ícone ao abrir/fechar o menu.
-
-    if(MobileMenu.classList.contains('active')){
-        Overlay.style.display='block'
-    }else{
-        Overlay.style.display='none'
+    if (MobileMenu.classList.contains('active')) {
+        Overlay.classList.add('active');
+    } else {
+        Overlay.classList.remove('active');
     }
-    
-})
+});
+
