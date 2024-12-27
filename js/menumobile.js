1c52795a547e46906d2b64e7755e64b690f048c0
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-27 12:22:20 -0300

    Menu mobile feito

--- js/menumobile.js
+++ js/menumobile.js
@@ -1,6 +1,15 @@
-// $(document).ready(()=>{
-//     $('#mobile-btn').on('click',()=>{
-//         $('#mobile-menu').toggleClass('active')
-//         $('#mobile-btn').find('i').toggleClass('fa-x')
-//     })
-// })
+const MobileBtn = document.getElementById('mobile-btn') 
+const MobileMenu = document.getElementById('mobile-menu') 
+const Burguer = document.querySelector('.fa-bars') 
+
+MobileBtn.addEventListener('click', () => { 
+    // Adiciona um evento de clique ao botão `MobileBtn`.
+
+    MobileMenu.classList.toggle('active') 
+    // Alterna a classe 'active' no menu. 
+    // Isso é usado para abrir ou fechar o menu (normalmente associado a um estilo CSS).
+
+    Burguer.classList.toggle('fa-x') 
+    // Alterna entre o ícone de "barras" (`fa-bars`) e o ícone de "X" (`fa-x`).
+    // Isso muda a aparência do ícone ao abrir/fechar o menu.
+})
